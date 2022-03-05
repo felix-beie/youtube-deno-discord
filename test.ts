9f59bd163582423b99e00e5babfa209311796c0b
treater21 <flx.beie@gmail.com>
2022-03-05 12:01:28 -0500

    init test.ts

---
+++ test.ts
@@ -0,0 +1,7 @@
+import { assert } from "./deps.ts"
+
+const GOOGLE_API_KEY = Deno.env.get("GOOGLE_API_KEY")
+
+Deno.test("check if api key is loaded correctly", () => {
+    assert(typeof GOOGLE_API_KEY === 'string', `GOOGLE_API_KEY type is ${typeof GOOGLE_API_KEY} instead of string.`)
+})
